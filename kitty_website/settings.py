c4297b990455656110bcea0af103c30de6d4a49e
jackberrypassionfruit <pashayanjack@gmail.com>
2026-02-26 17:52:02 -0800

    remove docker-compoise app bind mount

--- kitty_website/settings.py
+++ kitty_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     # "localhost",
     # "127.0.0.1",
     # "192.168.2.108",
+    # "*.iamnotgerman.de",
     # "letsadopt-me.iamnotgerman.de",
     # "letsadopt.wahoo-broadnose.ts.net",
     # "kitty_website",
